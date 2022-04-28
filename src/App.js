6b83b753f208fb550f5c17746b5133b1b8d23ab8
goatstash <tobiaspeterbedford@hotmail.com>
2022-04-28 14:23:52 +1000

    first commit App.js stylings

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import styled from "styled-components";
+import movieicon1 from "./icons/movieicon1.png";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: black;
+  justify-content: space-between;
+  color: white;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const MovieImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  justify-content: space-evenly;
+`;
+function App() {
+  return (
+    <Container>
+      <Header>
+        <AppName>
+          <MovieImage src={movieicon1} />
+          Prince's Theatre
+        </AppName>
+      </Header>
+      <MovieListContainer></MovieListContainer>
+    </Container>
+  );
+}
+
+export default App;
